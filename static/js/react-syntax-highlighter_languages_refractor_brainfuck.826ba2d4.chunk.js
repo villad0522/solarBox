3522643d347ceea205b842c643bccdfcf14e8b1b
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2021-10-14 13:07:49 +0900

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_brainfuck.826ba2d4.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpsolarbox=this.webpackJsonpsolarbox||[]).push([[18],{307:function(a,e,t){"use strict";function n(a){a.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}a.exports=n,n.displayName="brainfuck",n.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.826ba2d4.chunk.js.map
