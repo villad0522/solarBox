493f56d7a43eb81139d899d24d40219fd4de0ed8
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2021-10-15 14:27:25 +0900

    Updates

--- static/js/react-syntax-highlighter_languages_refractor_ini.5c9aba27.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpsolarbox=this.webpackJsonpsolarbox||[]).push([[60],{345:function(t,s,n){"use strict";function a(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=a,a.displayName="ini",a.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.5c9aba27.chunk.js.map
