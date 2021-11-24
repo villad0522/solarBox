afa2ccfca56865aec3ce1c392b82d305d4c1a3db
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2021-11-24 14:50:08 +0900

    Updates

--- static/js/react-syntax-highlighter_languages_refractor_arff.0de7f8ab.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpsolarbox=this.webpackJsonpsolarbox||[]).push([[8],{373:function(a,t,e){"use strict";function n(a){a.languages.arff={comment:/%.*/,string:{pattern:/(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,greedy:!0},keyword:/@(?:attribute|data|end|relation)\b/i,number:/\b\d+(?:\.\d+)?\b/,punctuation:/[{},]/}}a.exports=n,n.displayName="arff",n.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_arff.0de7f8ab.chunk.js.map
--- static/js/react-syntax-highlighter_languages_refractor_hsts.da796e3d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpsolarbox=this.webpackJsonpsolarbox||[]).push([[55],{415:function(a,s,e){"use strict";function t(a){a.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}a.exports=t,t.displayName="hsts",t.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.da796e3d.chunk.js.map
