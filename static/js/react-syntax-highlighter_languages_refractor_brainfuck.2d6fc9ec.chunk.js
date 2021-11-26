538c67c7c9cfcc6557c7f5c14460c42da730eda7
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2021-11-26 13:23:43 +0900

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_brainfuck.2d6fc9ec.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpsolarbox=this.webpackJsonpsolarbox||[]).push([[18],{383:function(a,e,t){"use strict";function n(a){a.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}a.exports=n,n.displayName="brainfuck",n.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.2d6fc9ec.chunk.js.map
---
+++ static/js/react-syntax-highlighter_languages_refractor_ini.b4a54237.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpsolarbox=this.webpackJsonpsolarbox||[]).push([[60],{421:function(t,s,n){"use strict";function a(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=a,a.displayName="ini",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.b4a54237.chunk.js.map
